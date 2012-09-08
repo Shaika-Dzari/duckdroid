c2f621194668158dcbeedb2518a65165ce069258
Shaika-Dzari <shaikadzari@4nakama.net>
2012-09-07 22:01:41 -0400

    Add some basic resource and start playing with the layout

--- src/net/nakama/duckdroid/Main.java
+++ src/net/nakama/duckdroid/Main.java
@@ -3,18 +3,14 @@ package net.nakama.duckdroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
 
 public class Main extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
     }
 }
